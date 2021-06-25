68c7461b95292a79ff5f63908c7749cb01ac06c0
yangyang <575001446@qq.com>
2021-06-25 08:15:42 +0800

    add 不同环境的配置文件

---
+++ config/chainWebpack.js
@@ -0,0 +1,22 @@
+export default config => {
+  config.module.rules.delete('svg');
+  config.resolve.alias
+    .set('utils', require.resolve('../utils/utils.jsx'))
+    .set('components', require.resolve('../src/components'));
+  config.module
+    .rule('svg')
+    .test(/\.svg(\?v=\d+\.\d+\.\d+)?$/)
+    .use([
+      {
+        loader: 'babel-loader',
+      },
+      {
+        loader: '@svgr/webpack',
+        options: {
+          babel: false,
+          icon: false,
+        },
+      },
+    ])
+    .loader(require.resolve('@svgr/webpack'));
+};
